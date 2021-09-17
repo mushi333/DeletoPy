0750895faea8c071d92839a57727358ef2729c9b
mushi333 <mujtaba.zahidi24@gmail.com>
2021-09-17 14:47:06 +1000

    Can delete certain file type extensions now in the same folder

--- app.py
+++ app.py
@@ -1,24 +1,37 @@
-import os
+"""
+A python cmd prompt application to delete certain file types in your folder
+"""
 
-folder = "tmp"
-file = "asdf.txt"
-current_file_path = folder + "/" + file
+import os, glob
+
+# folder = "tmp"
+# file_type = "asdf.txt"
+# current_file_path = folder + "/" + file_type
+global_file_type = "jpg"
+global_glob_files = glob.glob('*.' + global_file_type, recursive=True)
 
 
 class Delete:
-    def __init__(self, file_path):
-        self.file_path = file_path
+    def __init__(self, glob_files):
+        self.glob_files = glob_files
 
     def delete_file(self):
-        try:
-            os.remove(self.file_path)
-            print("File was successfully deleted")
-        except FileNotFoundError:
-            print("File does not exist or could not be found")
+        if self.glob_files == []:
+            print("No specified file type: " + global_file_type + " was found")
+            return
+
+        for file in self.glob_files:
+            try:
+                print("Found file: " + file)
+                print("Attempting to delete file: " + file)
+                os.remove(file)
+                print("File: " + file + " was deleted succesfully")
+            except OSError as exception:
+                print("Error: %s : %s" % (file, exception.strerror))
 
 
 if __name__ == '__main__':
-    delete_instance = Delete(current_file_path)
+    delete_instance = Delete(global_glob_files)
     delete_instance.delete_file()
     print("Exiting app")
     exit()
